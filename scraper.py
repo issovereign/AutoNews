a641b863bddfdc2de65e06d05124602288fbb4df
Peng_Jia_Wang <aaa7788h@gmail.com>
2023-08-15 22:22:55 +0800

    scraper_v2
    已成功可以讀取整個頁面的新聞，並且蒐集標題與內文，最後再創成一個csv檔

--- scraper.py
+++ scraper.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import time
 import pandas as pd
+import csv
 
 
 
@@ -14,26 +15,58 @@ options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 chrome = webdriver.Chrome(options=options)
 chrome.get("https://www.inside.com.tw/")
+chrome.maximize_window()
 
-# time.sleep(3)
+### KeyWord Search
 search_icon = chrome.find_element(By.CLASS_NAME, 'js-search_submit.search_submit.icon-search')
 search_icon.click()
 search = chrome.find_element(By.ID, 'search')
 time.sleep(1)
 search.send_keys('Musk', Keys.ENTER)
 
+### Article List
+time.sleep(2)
+nums = len(chrome.find_elements(By.CLASS_NAME, 'post_list_item'))
+page = 0
 
-titles = chrome.find_elements(By.CLASS_NAME, 'post_title')
+article_dict = {}
+article_dict['title'] = []
+article_dict['article'] = []
 
-title = titles[0]
-title.click()
 
-titlename = chrome.find_element(By.CLASS_NAME, 'post_header_title.js-auto_break_title').text
-print(titlename, '\n')
-content = chrome.find_elements(By.CLASS_NAME, 'ck-section')
-for cont in content:
-    print(cont.text, '\n')
+# while page != 0:
+for i in range(2):
+    titles = chrome.find_elements(By.CLASS_NAME, 'post_list_item')
+    title = titles[i]
+    t = title.find_element(By.CLASS_NAME, 'js-auto_break_title ')
+    t.click()
 
+    titlename = chrome.find_elements(By.CLASS_NAME, 'post_header_title.js-auto_break_title')[0].text
+    article_dict['title'].append(titlename)
+
+    print(titlename, '\n')
+    content = chrome.find_elements(By.CLASS_NAME, 'ck-section')
+    article = ""
+    for cont in content:
+        if "加入 INSIDE 會員" in cont.text:
+            break
+        print(cont.text, '\n')
+        article = article + cont.text
+    
+    article_dict['article'].append(article)
+
+    time.sleep(3)
+    chrome.back()
+    time.sleep(1)
+
+    chrome.execute_script("window.scrollBy(0, 300);")
+
+# chrome.find_element(By.CLASS_NAME, 'pagination_item-next-wrapper').click()
+# page += 1
+a = pd.DataFrame(article_dict)
+csvPath = 'test.csv'
+a.to_csv(csvPath, encoding='utf_8_sig')
+print(pd.DataFrame(article_dict))
 # soup = BeautifulSoup(chrome.page_source, 'html.parser')
 # titles = soup.find_all('div', class_='post_list_item_content')
 # # print(titles)
