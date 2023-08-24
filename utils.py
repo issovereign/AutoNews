89ba3085a8e5218883524ebec7a1a492234906b9
issovereign <108223085+issovereign@users.noreply.github.com>
2023-08-24 17:44:34 +0800

    load csv / single_article_get_keywords

--- utils.py
+++ utils.py
@@ -1,10 +1,41 @@
-# def load csv and return data
-def load_csv(path="scraper.csv"):
-    pass
+import csv
 
-def single_article_get_keywords(article, model, tokenizer, threshold=0.8):
-    pass
+def load_csv(file_path="scraper.csv"):
+    news_dict = {}
+    
+    with open(file_path, 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            news_dict[row['title']] = row['article']
+    
+    return news_dict
 
+def normalize(lst):
+    min_val = min(lst)
+    max_val = max(lst)
+    return [(x - min_val) / (max_val - min_val) for x in lst]
+
+def single_news_get_keywords(news, sentence_processor, threshold=0.8):
+    
+    title, article = news
+    
+    # get title's and article's tokens and embeddings
+    title_tokens = sentence_processor.sentences2tokens(title)
+    article_tokens = sentence_processor.sentences2tokens(article)
+    news_tokens_embeddings = sentence_processor.tokens2embeddings(title_tokens + article_tokens)
+
+    # use article embedding to compare every tokens
+    # find out those tokens should be keep as keywords
+    article_embedding = sentence_processor.tokens2embeddings(article)
+    article2news_similarities = sentence_processor.sentences_similarity(article_embedding + news_tokens_embeddings)
+    article2news_similarities = normalize(article2news_similarities)
+    
+    keywords = []
+    for token, simi in zip((title_tokens + article_tokens), article2news_similarities):
+        if simi >= threshold:
+            keywords.append(token)
+    
+    return keywords
 
 # def call gpt to generate articles
 
