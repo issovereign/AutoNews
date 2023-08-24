dc8a25f217c368586b73172fa1fd56eb4fa4eaba
issovereign <108223085+issovereign@users.noreply.github.com>
2023-08-24 12:28:23 +0800

    utils func def

--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
 # def load csv and return data
+def load_csv(path="scraper.csv"):
+    pass
+
+def single_article_get_keywords(article, model, tokenizer, threshold=0.8):
+    pass
+
 
 # def call gpt to generate articles
 
